7fec7a167a277839b91501d7dab5a8f642720cbd
Alvaro Lopez <alvarogelg@gmail.com>
2018-03-16 19:17:49 +0000

    resolving conflict

--- plugins/backoffice/handlers.js
+++ plugins/backoffice/handlers.js
@@ -1,13 +1,18 @@
+const sempahoreApiClient = require('integrations/semaphore');
+const db = require('data');
 
 module.exports = {};
 
 module.exports.dashboardView = {
 	description: 'Serves dashboard',
 	handler: async (request, h) => {
-		
+		const existingProjects = await sempahoreApiClient.listOrganizationProjects();
+		const possibleProjects = await db.get();
+		console.log(existingProjects);
+		console.log(possibleProjects);
 		const data = {
-			title: 'Yeyy',
-			message: 'Test me'
+			projects: existingProjects,
+			freeProjects: possibleProjects,
 		};
 
     return h.view('dashboard', data);
