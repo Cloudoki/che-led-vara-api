78711452220af1750406e207d30afc516e0e3b3e
Miguel Sousa <miguel@cloudoki.com>
2018-03-16 19:30:38 +0000

    Enabling projects plugin

--- plugins/index.js
+++ plugins/index.js
@@ -2,8 +2,6 @@ module.exports = [
 	require('inert'),
 	require('vision'),
 	require('plugins/media'),
-	// require('plugins/projects'),
-	require('plugins/backoffice'),
-	//require('plugins/realTime'),
-	//require('plugins/semaphore')
+	require('plugins/projects'),
+	require('plugins/backoffice')
 ];
