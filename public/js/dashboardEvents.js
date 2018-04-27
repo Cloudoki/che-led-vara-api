170e831e497134ae3b1f62fb0678ad6299951bbf
Alvaro Lopez <alvarogelg@gmail.com>
2018-04-27 16:45:43 +0100

    last version 1 of FE

--- public/js/dashboardEvents.js
+++ public/js/dashboardEvents.js
@@ -1,8 +1,9 @@
-function removeProject(evt) {
-    var data = $(this).parent().data();
-    var led = data.led;
-    var id = data.id;
-    var title = data.title
+function removeProject() {
+    var data = $(this).parent().data(),
+        led = data.led,
+        id = data.id,
+        title = data.title;
+
     $("#project_select_box").append(
         $("<option></option>")
             .attr("value",id)
@@ -12,15 +13,18 @@ function removeProject(evt) {
 }
 
 function addProject(hooksUrl) {
-    var led = $("#led_number").val();
-    var ledMonitor = $("#led_monitor").val() ? $("#led_monitor").val() : 0;
-    var ledDeploy = $("#led_deploy").val() ? $("#led_deploy").val() : 0;
-    var monitorUrl = $("#ping_url").val() ? $("#ping_url").val() : "";
-    var selectedTitle = $("#project_select_box").find(":selected").text();
-    var selectedEnvironment = $("#environment_select_box").find(":selected");
-    var error = 0;
+    var led = $("#led_number").val(), 
+        ledMonitor = $("#led_monitor").val() ? $("#led_monitor").val() : 0,
+        ledDeploy = $("#led_deploy").val() ? $("#led_deploy").val() : 0,
+        monitorUrl = $("#ping_url").val() ? $("#ping_url").val() : "",
+        selectedTitle = $("#project_select_box").find(":selected").text(),
+        selectedEnvironment = $("#environment_select_box").find(":selected"),
+        removeProjectFromList = 0;
+        error = 0;
     if(led) {
         $(".projects_with_leds").each(function (element) { 
+            if(selectedTitle == $(this).data("title"))
+                removeProjectFromList = removeProjectFromList + 1;
             if(selectedTitle == $(this).data("title") && selectedEnvironment.val() == $(this).data("environment"))
                 error = "That project is already in the above list";
             if(
@@ -37,40 +41,58 @@ function addProject(hooksUrl) {
             )  
                 error = "Led invalid";
         }); 
-        if(led == ledMonitor || led == ledDeploy || ledDeploy == ledMonitor)
+        if(led == ledMonitor || led == ledDeploy || (ledDeploy == ledMonitor && ledDeploy != 0))
             error = "Leds can't be equal";
         if(!error) {
-            var itemClass = $(".projects_with_leds").length % 2 == 0 ? "list-group-item-info" : "list-group-item-secondary";
-            var selectedProject = $("#project_select_box").find(":selected");
-            var uuid = guid();
-            var a = $("<a></a>")
-                .attr("data-led", led)
-                .attr("data-ledmonitor", ledMonitor)
-                .attr("data-leddeploy", ledDeploy)
-                .attr("data-monitorUrl", monitorUrl)
-                .attr("data-uuid", uuid)
-                .attr("data-id", selectedProject.val())
-                .attr("data-environment", selectedEnvironment.val())
-                .attr("data-title",selectedProject.text())
-                .attr("class","list-group-item " + itemClass + " projects_with_leds");
-            var divItem = $("<div></div>")
-                .attr("class","bmd-list-group-col");
-            var pTitle = $("<p></p>")
-                .attr("class","list-group-item-heading")
-                .text(selectedProject.text()+ " [" + selectedEnvironment.text() + "]");
-            var pSubheading = $("<p></p>")
-                .attr("class","list-group-item-text")
-                .text(hooksUrl + uuid);
-            var close = $("<span></span>")
-                .attr("class","label label-default label-pill")
-                .text("X")
-                .click(removeProject);
+            var itemClass = $(".projects_with_leds").length % 2 == 0 ? "list-group-item-info" : "list-group-item-secondary"
+                selectedProject = $("#project_select_box").find(":selected")
+                uuid = guid(),
+                a = $("<a></a>")
+                    .attr("data-led", led)
+                    .attr("data-ledmonitor", ledMonitor)
+                    .attr("data-leddeploy", ledDeploy)
+                    .attr("data-monitorUrl", monitorUrl)
+                    .attr("data-uuid", uuid)
+                    .attr("data-id", selectedProject.val())
+                    .attr("data-environment", selectedEnvironment.val())
+                    .attr("data-title",selectedProject.text())
+                    .attr("class","list-group-item " + itemClass + " projects_with_leds"),
+                divItem = $("<div></div>")
+                    .attr("class","bmd-list-group-col"),
+                pTitle = $("<p></p>")
+                    .attr("class","list-group-item-heading")
+                    .text(selectedProject.text()+ " [" + selectedEnvironment.text() + "]"),
+                pSubheading = $("<p></p>")
+                    .attr("class","list-group-item-text")
+                    .text(hooksUrl + uuid),
+                pBadges = $("<p></p>")
+                    .attr("class","list-group-item-text"),
+                badgeLed = $('<span></span>')
+                    .attr("class","badge badge-success")
+                    .text("Build LED: "+led),
+                badgeMonitor = $('<span></span>')
+                    .attr("class","badge badge-danger")
+                    .text("Monitor LED: "+ledMonitor),
+                badgeDeploy = $('<span></span>')
+                    .attr("class","badge badge-warning")
+                    .text("Deploy LED: "+ledDeploy),
+                close = $("<span></span>")
+                    .attr("class","label label-default label-pill")
+                    .text("X")
+                    .click(removeProject);
+            badgeLed.appendTo(pBadges);
+            if(ledMonitor)
+                badgeMonitor.appendTo(pBadges);
+            if(ledDeploy)
+                badgeDeploy.appendTo(pBadges);
             pTitle.appendTo(divItem);
             pSubheading.appendTo(divItem);
+            pBadges.appendTo(divItem);
             divItem.appendTo(a);
             close.appendTo(a);
             a.appendTo("#project_list");
-            selectedProject.remove();
+            if(removeProjectFromList == ($("#environment_select_box").children('option').length - 1))
+                selectedProject.remove();
             error = 0;
             $(".error").text("").hide();
             $("#led_number").val("");
@@ -89,15 +111,15 @@ function addProject(hooksUrl) {
 function saveProjects() {
     var data = [];
     $(".projects_with_leds").each(function (index) { 
-        var ledMonitor = $(this).data("ledmonitor");
-        var leddeploy = $(this).data("leddeploy");
-        var url = $(this).data("monitorurl");
-        var dataElement = {
-            id: $(this).data("uuid"),
-            ledBuild: $(this).data("led"),
-            environment: $(this).data("environment"),
-            name: $(this).data("title")
-        };
+        var ledMonitor = $(this).data("ledmonitor"),
+            leddeploy = $(this).data("leddeploy")
+            url = $(this).data("monitorurl"),
+            dataElement = {
+                id: $(this).data("uuid"),
+                ledBuild: $(this).data("led"),
+                environment: $(this).data("environment"),
+                name: $(this).data("title")
+            };
         if(ledMonitor) dataElement.ledMonitor = ledMonitor;
         if(leddeploy) dataElement.ledDeploy = leddeploy;
         if(url) dataElement.monitorUrl= url
